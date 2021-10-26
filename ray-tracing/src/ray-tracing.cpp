bf64373b2720cd1d9695580632c4304ee1d148b1
marcusmvfa <43256442+marcusmvfa@users.noreply.github.com>
2021-10-25 21:20:54 -0300

    livro 1 completo

--- ray-tracing/src/ray-tracing.cpp
+++ ray-tracing/src/ray-tracing.cpp
@@ -17,12 +17,15 @@
 #include "hittable_list.h"
 #include "sphere.h"
 
-
 #include "color.h"
 #include "ray.h"
 #include "camera.h"
 
 #include "material.h"
+
+#include "read_file_controller.h"
+
+#include <string>
 
 using namespace std;
 
@@ -107,39 +110,40 @@ void writeBitmapFile(ofstream &out, int width, int height) {
 	}
 }
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-        auto half_b = dot(oc, r.direction());
-        auto c = oc.length_squared() - radius*radius;
-        auto discriminant = half_b*half_b - a*c;
+double hit_sphere(const point3 &center, double radius, const ray &r) {
+	vec3 oc = r.origin() - center;
+	auto a = r.direction().length_squared();
+	auto half_b = dot(oc, r.direction());
+	auto c = oc.length_squared() - radius * radius;
+	auto discriminant = half_b * half_b - a * c;
 
-        if (discriminant < 0) {
-            return -1.0;
-        } else {
-            return (-half_b - sqrt(discriminant) ) / a;
-        }
+	if (discriminant < 0) {
+		return -1.0;
+	} else {
+		return (-half_b - sqrt(discriminant)) / a;
+	}
 }
 
-color ray_color(const ray& r, const hittable& world, int depth) {
-    hit_record rec;
-    // If we've exceeded the ray bounce limit, no more light is gathered.
-    if (depth <= 0)
-    	return color(0,0,0);
+color ray_color(const ray &r, const hittable &world, int depth) {
+	hit_record rec;
+	// If we've exceeded the ray bounce limit, no more light is gathered.
+	if (depth <= 0)
+		return color(0, 0, 0);
 
-    if (world.hit(r, 0.001, infinity, rec)) {
-    	ray scattered;
-    	        color attenuation;
-    	        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-    	            return attenuation * ray_color(scattered, world, depth-1);
-    	        return color(0,0,0);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+	if (world.hit(r, 0.001, infinity, rec)) {
+		ray scattered;
+		color attenuation;
+		if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+			return attenuation * ray_color(scattered, world, depth - 1);
+		return color(0, 0, 0);
+	}
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-void trace_lines(int thread_id, int start_line, int end_line, int image_width, int image_height, camera *cam, hittable_list world, image *img){
+void trace_lines(int thread_id, int start_line, int end_line, int image_width,
+		int image_height, camera *cam, hittable_list world, image *img) {
 	const int samples_per_pixel = 100;
 	const int max_depth = 50;
 
@@ -147,122 +151,176 @@ void trace_lines(int thread_id, int start_line, int end_line, int image_width, i
 //		outfile.open("image.ppm");
 
 	for (int j = start_line; j < end_line; j++) {
-			std::cerr << "\rThread: " << thread_id << "- Scanlines remaining: " << j << ' ' << std::flush;
-			for (int i = 0; i < image_width; ++i) {
-				color pixel_color(0, 0, 0);
-				for (int s = 0; s < samples_per_pixel; ++s) {
-					auto u = (i + random_double()) / (image_width-1);
-					auto v = (j + random_double()) / (image_height-1);
-					ray r = cam->get_ray(u, v);
-					pixel_color += ray_color(r, world, max_depth);
-				}
-//				write_color(outfile, pixel_color, samples_per_pixel);
-				img->set(i, image_height -j , pixel_color, samples_per_pixel);
+//		std::cerr << "\rThread: " << thread_id << "- Scanlines remaining: " << j
+//				<< ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i) {
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s) {
+				auto u = (i + random_double()) / (image_width - 1);
+				auto v = (j + random_double()) / (image_height - 1);
+				ray r = cam->get_ray(u, v);
+				pixel_color += ray_color(r, world, max_depth);
 			}
+//				write_color(outfile, pixel_color, samples_per_pixel);
+			img->set(i, image_height - j, pixel_color, samples_per_pixel);
 		}
+	}
 //	outfile.close();
 }
 
-void imprimir(int idthread, int numberline){
+void imprimir(int idthread, int numberline) {
 	std::cerr << 'id da thread:' << idthread << ' - ' << numberline;
+}
+
+hittable_list random_scene() {
+	hittable_list world;
+
+	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+	for (int a = -3; a < 3; a++) {
+		for (int b = -3; b < 3; b++) {
+			auto choose_mat = random_double();
+			point3 center(a + 0.9 * random_double(), 0.2,
+					b + 0.9 * random_double());
+
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+				shared_ptr<material> sphere_material;
+
+				if (choose_mat < 0.8) {
+					// diffuse
+					auto albedo = color::random() * color::random();
+					sphere_material = make_shared<lambertian>(albedo);
+					world.add(
+							make_shared<sphere>(center, 0.2, sphere_material));
+				} else if (choose_mat < 0.95) {
+					// metal
+					auto albedo = color::random(0.5, 1);
+					auto fuzz = random_double(0, 0.5);
+					sphere_material = make_shared<metal>(albedo, fuzz);
+					world.add(
+							make_shared<sphere>(center, 0.2, sphere_material));
+				} else {
+					// glass
+					sphere_material = make_shared<dielectric>(1.5);
+					world.add(
+							make_shared<sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
+
+	auto material1 = make_shared<dielectric>(1.5);
+	world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+	auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+	world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+	auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+	world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+	return world;
 }
 
 int main() {
 	// Image
-	const int samples_per_pixel = 100;
-		const int max_depth = 50;
-	    const auto aspect_ratio = 16.0 / 9.0;
-	    const int image_width = 360;
-	    const int image_height = static_cast<int>(image_width / aspect_ratio);
-	    const int number_threads = 8;
-	    const int lines_thread = image_height / number_threads;
-	    int start = 0;
-	    image img(image_width, image_height);
-	    //conjunto de threads
+    const int samples_per_pixel = 100;
+	const int max_depth = 35;
+	const auto aspect_ratio = 16.0 / 9.0;
+	    const int image_width = 260;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int number_threads = 8;
+	const int lines_thread = image_height / number_threads;
+	int start = 0;
+	image img(image_width, image_height);
+//	read_file_controller readFile;
+
+	//conjunto de threads
 //	    std::vector<std::thread> thread_vec;
 
-	    // Camera
-	    camera cam;
+	// Camera
+	point3 lookfrom(13, 2, 3);
+	point3 lookat(0, 0, 0);
+	vec3 vup(0, 1, 0);
+	auto dist_to_focus = 10.0;
+	auto aperture = 0.1;
 
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture,
+			dist_to_focus);
 
-	    // World
-	    hittable_list world;
-	    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	       auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-	       auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-	           auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+	// World
+	auto world = random_scene();
+	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+	auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+	auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+	auto material_die = make_shared<dielectric>(1.5);
 
-	       world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-	       world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-	       world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-	       world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+//	world.add(
+//			make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0,
+//					material_ground));
+//	world.add(make_shared<sphere>(point3(-1.0, 1.0, -1.5), 0.5, material_die));
+//	world.add(make_shared<sphere>(point3(-1.0, 1.0, -1.5), -0.4, material_die));
+//	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+//	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
-	    // Camera
-
-	    auto viewport_height = 2.0;
-	    auto viewport_width = aspect_ratio * viewport_height;
-	    auto focal_length = 1.0;
-
-	    auto origin = point3(0, 0, 0);
-	    auto horizontal = vec3(viewport_width, 0, 0);
-	    auto vertical = vec3(0, viewport_height, 0);
-	    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
-
-//	    unsigned char *img = new unsigned char[image_width * image_height * 3 * sizeof(int)];
-//	    	cel_size = sizeof(unsigned char) * 3;
-//	    	line_size = cel_size * image_width;
-
-	// Render
-
-	ofstream outfile;
-	outfile.open("imagem1.bmp");
-//	outfile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+//	for(int i = 0; i < readFile.number_of_forms; i++){
+//		world.add(readFile.getGeometry());
+//	}
 
 	//	#Colocar o esquema de ler o arquivo linha por linha
 
+	for (int imag = 1; imag < 30; imag++) {
 
-	//Thread
+		const int angle = 40;
 
-	std::vector<std::thread> threads;
+		point3 lookfrom(-imag + 10, 2, -imag + 5);
+		point3 lookat(0, 0, 0);
+		vec3 vup(0, 1, 0);
+		auto dist_to_focus = 10.0;
+		auto aperture = 0.1;
 
-	  for (int i = 0; i < number_threads ; i++) {
-		  if(i + 1 == number_threads)
-		  			//Colocar o objeto em memória
-		  			threads.push_back(thread(trace_lines, i, start, image_height, image_width, image_height, &cam, world, &img));
-		  		else
-//		  			threads.push_back(thread(imprimir, i, start));
-		  			threads.push_back(thread(trace_lines, i, start, start + lines_thread, image_width, image_height, &cam, world, &img));
+		camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture,
+				dist_to_focus);
 
-		  		start += lines_thread;
-	  }
+		std::cout << "\nNovaImagem" << imag << "\n";
 
-	  for (auto &th : threads) {
-	    th.join();
-	  }
+		std::string file_name = "imagens/imagem";
+		file_name += std::to_string(imag);
+		file_name += ".bmp";
 
-	  img.writeBitmapFile(outfile);
+		ofstream outfile;
+		outfile.open(file_name);
 
-//
-//
-//	for (int j = image_height - 1; j >= 0; --j) {
-//		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-//		for (int i = 0; i < image_width; ++i) {
-//			color pixel_color(0, 0, 0);
-//			for (int s = 0; s < samples_per_pixel; ++s) {
-//				auto u = (i + random_double()) / (image_width-1);
-//				auto v = (j + random_double()) / (image_height-1);
-//				ray r = cam.get_ray(u, v);
-//				pixel_color += ray_color(r, world, max_depth);
-//			}
-//			write_color(outfile, pixel_color, samples_per_pixel);
-//
-//		}
-//	}
+		//Thread
+
+		std::vector<std::thread> threads;
+
+		for (int i = 0; i < number_threads; i++) {
+			if (i + 1 == number_threads)
+				//Colocar o objeto em memória
+				threads.push_back(
+						thread(trace_lines, i, start, image_height, image_width,
+								image_height, &cam, world, &img));
+			else
+				//		  			threads.push_back(thread(imprimir, i, start));
+				threads.push_back(
+						thread(trace_lines, i, start, start + lines_thread,
+								image_width, image_height, &cam, world, &img));
+
+			start += lines_thread;
+		}
+
+		for (auto &th : threads) {
+			th.join();
+		}
+
+		start = 0;
+
+		img.writeBitmapFile(outfile);
+		outfile.close();
+	}
+
 	std::cerr << "\nDone.\n";
-//
-	outfile.close();
-//
-//	outfile.open("image.bmp", ios::binary | ios::out);
-//	writeBitmapFile(outfile, image_width, image_height);
-//	outfile.close();
+
 }
